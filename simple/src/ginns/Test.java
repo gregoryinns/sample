c5254e00ad1062d606d61b749bad5c584593f803
gregoryinns <gwinns@gmail.com>
2013-11-20 08:15:28 -0500

    Initial commit

---
+++ simple/src/ginns/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ginns;
+
+/**
+ * @author gregoryinns
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a quick test");
+	}
+
+}
